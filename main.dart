0b083680fa69339f2a90d5fe496f6f0538f07dd1
DavincyProject <76769657+DavincyProject@users.noreply.github.com>
2022-10-19 12:04:27 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 import 'package:aplikasi_bioskop/UI/Login.dart';
-import 'package:aplikasi_bioskop/UI/Register.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Nexa'),
